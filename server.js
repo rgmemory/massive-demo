bc3921c7838082cd6074e5546a2bd482b52430ca
Russell Memory <rmemory@Russells-MBP.lan>
2017-03-17 23:22:59 -0600

    stuff

--- server.js
+++ server.js
@@ -1,15 +1,38 @@
 var express = require('express');
 var bodyParser = require('body-parser');
 var cors = require('cors');
+var massive = require('massive');
+
+var massiveServer = massive.connectSync({
+  connectionString: "postgres://localhost/massive-demo"
+});
+
+
 
 var app = express();
 app.use(bodyParser.json());
 
+app.set('db', massiveServer);
+
+var db = app.get('db');
+
+console.log(db);
+
 var port = 3000;
 
+
+
+
 app.get('/incidents', function(req, res) {
-  console.log('POST sighting');
+
+  db.get_all_incidents(function(err, incidents){
+    res.send(incidents);
+  });
 });
+
+
+
+
 
 app.post('/incidents', function(req, res) {
   console.log('POST sighting');
